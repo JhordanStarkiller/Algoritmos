219e221aa499ac3e520b19a30407a64601b9d3da
CarlosCartagenaDeveloper <85804802+CarlosCartagenaDeveloper@users.noreply.github.com>
2021-10-07 10:48:36 -0400

    Add files via upload

---
+++ Ejercicios propuestos 2/2.13.py
@@ -0,0 +1,7 @@
+sueldo = float (input ('S: '))
+semanas_por_mes=4
+meses_por_ano=12
+ahorros=sueldo*0.15+semanas_por_mes*meses_por_ano
+print (ahorros)
+#print (meses_por_ano)
+#print (semanas_por_mes)
---
+++ Ejercicios propuestos 2/2.7.py
@@ -0,0 +1,4 @@
+costo_kilometro = float (input ('CKm: '))
+kilometros_por_recorrer = float (input ('KM: '))
+costo_del_boleto=kilometros_por_recorrer*costo_kilometro
+print (costo_del_boleto)
