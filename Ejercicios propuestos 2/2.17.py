1ac49ff4b4ce12940e0bf9585febb8a540186db2
Jhordan Soto Mercado <erick.soto@uab.edu.bo>
2021-10-11 14:05:04 -0400

    jhordan

--- Ejercicios propuestos 2/2.17.py
+++ Ejercicios propuestos 2/2.17.py
@@ -1,17 +1,6 @@
-INICIO.
-Imprimir “Confección de una prenda de vestir”
-Ingrese Talla.
-Ingrese Largo.
-Ingrese Ancho.
-Ingrese Cintura.
-Ingrese Cuello.
-Dibujar las medidas en la tela.
-Cortar por las medidas del dibujo.
-Coser las partes delantera y trasera.
-Cortar hojales.
-Coser hojales.
-Pegar botones.
-Coser las etiquetas.
-Realizar Control de Calidad.
-Prenda Lista.
-FIN.
+T = float(input("Talla:"))
+L = float(input("Largo:"))
+A = float(input("Ancho:"))
+CI = float(input("Cintura:"))
+CU = float(input("Cuello:"))
+print("Camisa Hecha de talla" , T)
